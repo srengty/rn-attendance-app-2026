784044b96f09057b56ba3d859c3a361bc8b7a31c
srengty <sreng.itc@gmail.com>
2026-01-08 18:56:48 +0700

    api to login

--- LoginScreen.js
+++ LoginScreen.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation } from "@react-navigation/native";
-import { Button, TextInput, View } from "react-native";
+import { Button, Text, TextInput, View } from "react-native";
 /*
 1. Fill in username and password fields
 2. Verify with api call
@@ -15,17 +15,18 @@ export default function LoginScreen(){
     const [errorMessage, setErrorMessage] = useState("");
     const handleLogin = async () => {
         try {
-            /* const response = await fetch("https://example.com/api/login", {
+            const response = await fetch("http://192.168.1.199:8000/api/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify({ username, password }),
-            }); */
-            const response = {
+                body: JSON.stringify(
+                    { email:username, password: password, device_name:'iphone17' }),
+            });
+            /* const response = {
                 ok: true, 
                 json: async () => ({ token: "dummy-auth-token" })
-            }; // Mock response for demonstration
+            }; */ // Mock response for demonstration
             const data = await response.json();
             if (response.ok) {
                 // Save auth token (for simplicity, using AsyncStorage here)
